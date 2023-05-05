b5d7cca33daefca2d8f6bd22205068dc82352c8d
Carl Larsson <calle.m.larsson@gmail.com>
2023-05-05 10:55:03 +0200

    config file for GA

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#==============================================================================
+# Author: Carl Larsson
+# Description: Config file
+# Date: 2023-05-05
+#==============================================================================
+
+
+#==============================================================================
+# Initialize the global variable model for the ANN, needed for GA
+#def init():
+global model
+model = None
+global ga_data
+ga_data = None
+global ga_labels
+ga_labels = None
+#==============================================================================
